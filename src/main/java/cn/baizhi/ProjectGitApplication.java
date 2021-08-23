2df7f230ae420ec2191453d05c80b42f066c91d4
yinhuiquan <1258836412@qq.com>
2021-08-23 20:19:50 +0800

    本地项目推送至Github

---
+++ src/main/java/cn/baizhi/ProjectGitApplication.java
@@ -0,0 +1,13 @@
+package cn.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectGitApplication.class, args);
+    }
+
+}
